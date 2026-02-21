312926509fa3216198df6be0dcaffa67de078dde
Hamza Saddouki <hsaddouki@gmail.com>
2026-02-21 19:20:40 +0100

    Fix deprecated LangChain imports in vector_client.py

--- src/database/vector_client.py
+++ src/database/vector_client.py
@@ -1,6 +1,6 @@
-from langchain_community.embeddings import OllamaEmbeddings
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain_core.documents import Document
+from langchain_ollama import OllamaEmbeddings
 
 from config import settings
 
